a440d000f38360c440b453bff58e1cab6410b9e4
Lukasz Konatowski <konat1993@gmail.com>
2021-11-19 23:52:59 +0100

    unlocked POST request

--- src/api/postRequest.js
+++ src/api/postRequest.js
@@ -2,14 +2,13 @@ import axios from 'axios'
 import dataToPost from './requestData'
 export const postRequest = (data) => {
     const newData = dataToPost(data)
-    console.log(newData)
-    // const request = axios.post("https://frosty-wood-6558.getsandbox.com:443/dishes",
-    //     newData
-    // ).then(data => console.log(data))
-    //     .catch(err => {
-    //         console.log("errrrrrr ", err)
-    //     })
-    // return request
+    const request = axios.post("https://frosty-wood-6558.getsandbox.com:443/dishes",
+        newData
+    ).then(data => console.log(data))
+        .catch(err => {
+            alert(err)
+        })
+    return request
 }
 
 export default postRequest
